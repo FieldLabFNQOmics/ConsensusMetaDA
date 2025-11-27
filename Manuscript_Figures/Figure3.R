abdbd4fe82e70a74afe23cac5e81bca87b26e64c
ManoKumar06 <manoharan.kumar@jcu.edu.au>
2025-11-28 09:35:55 +1000

    Update Figure3.R

--- Manuscript_Figures/Figure3.R
+++ Manuscript_Figures/Figure3.R
@@ -1,10 +1,14 @@
 ################### Figure 3 #####################
+wget ftp.microbio.me/emp/release1/otu_tables/deblur/emp_deblur_150bp.subset_2k.rare_5000.biom or 
 
+Download via https://ftp.microbio.me/emp/release1/otu_tables/deblur/ 
+
+file name: emp_deblur_150bp.subset_2k.rare_5000.biom
 
 ## emp
 biome_file <- "./emp_deblur_150bp.subset_2k.rare_5000.biom"
 
-sample_table_file <-  "./samples_table_emp.txt"
+sample_table_file <-  "../Data/samples_table_emp.txt"
 
 
 ## emp
